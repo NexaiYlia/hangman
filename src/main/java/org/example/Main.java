37ffc60adcf9052bdcc10f45ba895bd00338c9eb
NexaiYlia <nexai.ylia@gmail.com>
2024-11-26 21:49:56 -0500

    corrections

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,21 +2,19 @@ package org.example;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-import java.util.Scanner;
+import java.io.InputStream;
+import java.util.*;
+import java.util.stream.IntStream;
 
 public class Main {
 
-    public static final String PATH_NAME = "src/main/resources/words.txt";
+    public static final String GAME_WORDS_PATH_NAME = "src/main/resources/words.txt";
 
     public static void main(String[] args) {
 
-
         List<String> words = new ArrayList<>();
         try {
-            Scanner scanner = new Scanner(new File(PATH_NAME));
+            Scanner scanner = new Scanner(new File(GAME_WORDS_PATH_NAME));
 
             while (scanner.hasNext()) {
                 words.add(scanner.nextLine());
@@ -26,19 +24,17 @@ public class Main {
         }
         Scanner keyboard = new Scanner(System.in);
 
-
         Random random = new Random();
         String word = words.get(random.nextInt(words.size()));
 
-        System.out.println(word);//чтобы легче было тестировать
+        System.out.println(word);
         List<Character> playerLetters = new ArrayList<>();
         printWordStars(word, playerLetters);
         int wrongCount = 0;
 
         while (true) {
 
-
-            if (!getPlayerLetter(keyboard, word, playerLetters)) {
+            if (!isLetterInWord(keyboard, word, playerLetters)) {
                 wrongCount++;
             }
 
@@ -48,23 +44,20 @@ public class Main {
                 System.out.println("You are a loser! Game over!");
                 break;
             }
-
             if (printWordStars(word, playerLetters)) {
                 System.out.println("You win!");
                 break;
             }
-
         }
-
     }
 
-    private static boolean getPlayerLetter(Scanner keyboard, String word, List<Character> playerGuesses) {
+    private static boolean isLetterInWord(Scanner keyboard, String word, List<Character> playerGuesses) {
         System.out.println("Enter a letter:");
 
         String letterGuess = keyboard.nextLine();
         playerGuesses.add(letterGuess.charAt(0));
 
-        return (word.contains(letterGuess));
+        return word.contains(letterGuess);
     }
 
 
@@ -82,33 +75,19 @@ public class Main {
         return (word.length() == correctCount);
     }
 
+    private static final Map<Integer, String> HANGMAN_PARTS = Map.of(
+            1, "  o  \n",
+            2, "/",
+            3, " \\ \n",
+            4, "  |  \n",
+            5, "/",
+            6, " \\ \n"
+    );
+
     private static void printMen(Integer wrongCount) {
         System.out.println("This word does not contain such a letter");
-        System.out.println("------");
-        System.out.println("  |   ");
-        if (wrongCount >= 1) {
-            System.out.println("  O  ");
-        }
-        if (wrongCount >= 2) {
-            System.out.print("/");
-            if (wrongCount >= 3) {
-                System.out.println("   \\");
-            } else {
-                System.out.println("");
-            }
-        }
-        if (wrongCount >= 4) {
-            System.out.println("  |  ");
-        }
-        if (wrongCount >= 5) {
-            System.out.print("/");
-            if (wrongCount >= 6) {
-                System.out.println("   \\");
-            } else {
-                System.out.println("");
-            }
-        }
-
-        System.out.println();
+        StringBuilder hangmanDrawing = new StringBuilder();
+        IntStream.rangeClosed(1, wrongCount).forEach(i -> hangmanDrawing.append(HANGMAN_PARTS.get(i)));
+        System.out.println(hangmanDrawing);
     }
 }
